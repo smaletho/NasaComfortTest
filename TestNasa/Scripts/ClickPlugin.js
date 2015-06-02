15c51c9cc33e5711caa0622ecfade52c29352ff9
Tom Smale <toms@bgmedia.com>
2015-06-02 10:22:30 -0400

    testing click plugin

---
+++ TestNasa/Scripts/ClickPlugin.js
@@ -0,0 +1,68 @@
+﻿$.fn.tomclick = function (hoverClass, callback) {
+
+    var timeoutClick;
+    var didTouch = false;
+
+    $(this).on("touchstart", function () {
+        //event.preventDefault();
+        didTouch = true;
+
+        console.log("touchstart");
+            
+        $(this).addClass(hoverClass);
+
+        timeoutClick = setTimeout(function () {
+            console.log("timeout!");
+            $(this).removeClass(hoverClass);
+        }, 1000);
+
+    });
+
+    $(this).on("touchend", function () {
+        //event.preventDefault();
+        didTouch = true;
+        if (timeoutClick) {
+
+            $(this).removeClass(hoverClass);
+            clearTimeout(timeoutClick);
+            console.log("touchend");
+            callback(this);
+        }
+    });
+
+    $(this).on("mousedown", function () {
+        if (!didTouch) {
+            console.log("mousedown");
+
+            $(this).addClass(hoverClass);
+
+            timeoutClick = setTimeout(function () {
+                console.log("timeout!");
+                $(this).removeClass(hoverClass);
+            }, 1000);
+        }
+    });
+
+    $(this).on("mouseup", function () {
+
+        if (!didTouch) {
+            if (timeoutClick) {
+
+                $(this).removeClass(hoverClass);
+                clearTimeout(timeoutClick);
+                console.log("mouseup");
+                callback(this);
+            }
+        }
+    });
+
+    //$(this).on("mouseover", function () {
+    //    $(this).addClass(hoverClass);
+    //});
+
+    //$(this).on("mouseout", function () {
+    //    $(this).removeClass(hoverClass);
+    //});
+
+    
+};
